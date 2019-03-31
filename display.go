10a3f7fbd011aae87417d6af976625c35b726113
Bradley Cicenas <bradley@vektor.nyc>
2019-03-31 14:48:21 +0000

    init color picker

---
+++ display.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"math"
+	"sync"
+
+	"github.com/gdamore/tcell"
+	"github.com/teacat/noire"
+)
+
+const (
+	padding       = 2
+	navIncr uint8 = 5
+	navMax  uint8 = 200
+	navMin  uint8 = 0
+)
+
+type Display struct {
+	rgb        []int32
+	hues       []tcell.Color  // hue nav
+	mHues      []tcell.Color  // minimap hues
+	xHues      []*noire.Color // base hues
+	saturation uint8          // 0 to 200
+	brightness uint8          // 0 to 200
+	pos        int
+	center     int
+	width      int
+	screen     tcell.Screen
+	lock       sync.RWMutex
+}
+
+func NewDisplay(s tcell.Screen) *Display {
+	d := &Display{
+		screen: s,
+	}
+	d.mkhues()
+	d.Reset()
+	return d
+}
+
+func (d *Display) Reset() {
+	d.saturation = 100
+	d.brightness = 100
+	d.Resize()
+	d.pos = d.center
+}
+
+func (d *Display) Saturation() float64   { return (float64(d.saturation) / 100) - 1 }
+func (d *Display) Brightness() float64   { return (float64(d.brightness) / 100) - 1 }
+func (d *Display) Selected() tcell.Color { return d.hues[d.pos] }
+
+func (d *Display) Hues() []tcell.Color {
+	l := d.pos - d.center
+	r := d.pos + d.center
+	hlen := len(d.hues)
+	if l < 0 {
+		return append(d.hues[hlen+l:], d.hues[0:r]...)
+	}
+	if r > hlen {
+		return append(d.hues[l:hlen-1], d.hues[0:r-hlen]...)
+	}
+	return d.hues[l:r]
+}
+
+func (d *Display) Resize() {
+	w, _ := d.screen.Size()
+	d.width = w - (padding * 3)
+	d.center = d.width / 2
+	d.build()
+}
+
+func (d *Display) mkhues() {
+	var (
+		incr float64 = 1
+		r    float64 = 255
+		g    float64 = 0
+		b    float64 = 0
+	)
+	d.xHues = append(d.xHues, noire.NewRGB(r, g, b))
+
+	for b < 255 {
+		b += incr
+		d.xHues = append(d.xHues, noire.NewRGB(r, g, b))
+	}
+	for r > 0 {
+		r -= incr
+		d.xHues = append(d.xHues, noire.NewRGB(r, g, b))
+	}
+	for g < 255 {
+		g += incr
+		d.xHues = append(d.xHues, noire.NewRGB(r, g, b))
+	}
+	for b > 0 {
+		b -= incr
+		d.xHues = append(d.xHues, noire.NewRGB(r, g, b))
+	}
+	for r < 255 {
+		r += incr
+		d.xHues = append(d.xHues, noire.NewRGB(r, g, b))
+	}
+	for g > 0 {
+		g -= incr
+		d.xHues = append(d.xHues, noire.NewRGB(r, g, b))
+	}
+}
+
+func applySaturation(level float64, c *noire.Color) *noire.Color {
+	if level < 0 {
+		return c.Desaturate(level * -1)
+	}
+	return c.Saturate(level)
+}
+
+func applyBrightness(level float64, c *noire.Color) *noire.Color {
+	if level < 0 {
+		return c.Darken(level * -1)
+	}
+	return c.Brighten(level)
+}
+
+func (d *Display) build() {
+	var tc tcell.Color
+	d.hues = d.hues[:0]
+	d.mHues = d.mHues[:0]
+	miniStep := len(d.xHues) / d.width
+
+	for idx, c := range d.xHues {
+		c = applySaturation(d.Saturation(), c)
+		c = applyBrightness(d.Brightness(), c)
+		r, g, b := c.RGB()
+		tc = tcell.NewRGBColor(int32(r), int32(g), int32(b))
+		d.hues = append(d.hues, tc)
+		if idx == 0 || idx%miniStep == 0 {
+			d.mHues = append(d.mHues, tc)
+		}
+	}
+}
+
+func (d *Display) Pos() int  { return d.pos }
+func (d *Display) HPos() int { return int(math.Mod(float64(d.pos), 51.0)) }
+
+func (d *Display) SaturationUp() (ok bool) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	if d.saturation == navMax {
+		return false
+	}
+	d.saturation += navIncr
+	d.build()
+	return true
+}
+
+func (d *Display) SaturationDown() (ok bool) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	if d.saturation == navMin {
+		return false
+	}
+	d.saturation -= navIncr
+	d.build()
+	return true
+}
+
+func (d *Display) HueUp(step int) (ok bool) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	d.pos += step
+	if d.pos >= len(d.hues)-1 {
+		d.pos -= len(d.hues) - 1
+	}
+	d.build()
+	return true
+}
+
+func (d *Display) HueDown(step int) (ok bool) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	d.pos -= step
+	if d.pos < 0 {
+		d.pos += len(d.hues) - 1
+	}
+	d.build()
+	return true
+}
+
+func (d *Display) BrightnessUp() (ok bool) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	if d.brightness == navMax {
+		return false
+	}
+	d.brightness += navIncr
+	d.build()
+	return true
+}
+
+func (d *Display) BrightnessDown() (ok bool) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	if d.brightness == navMin {
+		return false
+	}
+	d.brightness -= navIncr
+	d.build()
+	return true
+}
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gdamore/tcell"
+)
+
+var (
+	boxW = 120
+	boxH = 80
+	disp *Display
+)
+
+func draw(s tcell.Screen) {
+	w, h := s.Size()
+
+	if w == 0 || h == 0 {
+		return
+	}
+
+	lh := h / 2
+	lw := w / 2
+	lx := w / 4
+	ly := h / 4
+	st := tcell.StyleDefault
+	gl := ' '
+
+	st = st.Background(disp.Selected())
+
+	for row := 0; row < lh; row++ {
+		for col := 0; col < lw; col++ {
+			s.SetCell(lx+col, ly+row, st, gl)
+		}
+	}
+
+	s.SetCell(1, 1, tcell.StyleDefault, []rune(fmt.Sprintf("%03d %3.3f", disp.brightness, disp.Brightness()))...)
+	s.SetCell(1, 2, tcell.StyleDefault, []rune(fmt.Sprintf("%03d %3.3f", disp.saturation, disp.Saturation()))...)
+	r, g, b := disp.Selected().RGB()
+	s.SetCell(1, 3, tcell.StyleDefault, []rune(fmt.Sprintf("%03d %03d %03d", r, g, b))...)
+	s.SetCell(1, 4, tcell.StyleDefault, []rune(fmt.Sprintf("%04d", disp.pos))...)
+	s.SetCell(1, 5, tcell.StyleDefault, []rune(fmt.Sprintf("%04d", disp.center))...)
+
+	for col, color := range disp.Hues() {
+		st = st.Background(color)
+		s.SetCell(col+padding, ly+lh+1, st, gl)
+		s.SetCell(col+padding, ly+lh+2, st, gl)
+		if col == disp.center {
+			s.SetCell(col+padding, ly+lh, tcell.StyleDefault, '↓')
+			s.SetCell(col+padding, ly+lh+3, tcell.StyleDefault, '↑')
+		} else {
+			s.SetCell(col+padding, ly+lh, tcell.StyleDefault, ' ')
+			s.SetCell(col+padding, ly+lh+3, tcell.StyleDefault, ' ')
+		}
+	}
+
+	for col := range disp.mHues {
+		st = st.Background(disp.mHues[col])
+		s.SetCell(col+padding, ly+lh+4, st, gl)
+	}
+
+	s.Show()
+}
+
+func main() {
+	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	s.SetStyle(tcell.StyleDefault.
+		Foreground(tcell.ColorWhite).
+		Background(tcell.ColorDefault))
+	s.Clear()
+	disp = NewDisplay(s)
+
+	quit := make(chan struct{})
+	go func() {
+		for {
+			ev := s.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				switch ev.Key() {
+				case tcell.KeyRune:
+					switch ev.Rune() {
+					case 'r':
+						disp.Reset()
+						draw(s)
+					case 'l':
+						if ok := disp.SaturationUp(); ok {
+							draw(s)
+						}
+					case 'h':
+						if ok := disp.SaturationDown(); ok {
+							draw(s)
+						}
+					}
+				case tcell.KeyCtrlRightSq:
+					if ok := disp.HueUp(10); ok {
+						draw(s)
+					}
+				case tcell.KeyCtrlLeftSq:
+					if ok := disp.HueDown(10); ok {
+						draw(s)
+					}
+				case tcell.KeyRight:
+					if ok := disp.HueUp(10); ok {
+						draw(s)
+					}
+				case tcell.KeyLeft:
+					if ok := disp.HueDown(10); ok {
+						draw(s)
+					}
+				case tcell.KeyUp:
+					if ok := disp.BrightnessUp(); ok {
+						draw(s)
+					}
+				case tcell.KeyDown:
+					if ok := disp.BrightnessDown(); ok {
+						draw(s)
+					}
+				case tcell.KeyEscape, tcell.KeyEnter:
+					close(quit)
+					return
+				case tcell.KeyCtrlL:
+					s.Sync()
+				}
+			case *tcell.EventResize:
+				s.Sync()
+			}
+		}
+	}()
+
+	draw(s)
+
+loop:
+	for {
+		select {
+		case <-quit:
+			break loop
+		case <-time.After(time.Millisecond * 50):
+		}
+	}
+
+	w, h := s.Size()
+	s.Fini()
+	fmt.Printf("w=%d h=%d hues=%d\n", w, h, len(disp.hues))
+}
